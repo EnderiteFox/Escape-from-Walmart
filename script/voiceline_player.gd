61df694cae7f81fc6599e013c0efca88ea3a6d15
EndersteveGamer <85014963+EndersteveGamer@users.noreply.github.com>
2024-01-28 16:18:10 +0100

    Added voiceline player

--- script/voiceline_player.gd
+++ script/voiceline_player.gd
@@ -2,8 +2,16 @@ extends AudioStreamPlayer
 class_name VoicelinePlayer
 
 var alreadyPlayed := Dictionary()
+var sounds: Dictionary = {
+	WAIT_SHORT = [preload("res://voicelines/Pack 1/Level0_00_A.ogg")],
+	WAIT_LONG = [preload("res://voicelines/Pack 1/Level0_01_A.ogg")]
+}
 
-enum VOICELINES {
+enum VOICELINE {
 	WAIT_SHORT,
 	WAIT_LONG
 }
+
+func _ready() -> void:
+	for voiceline in VOICELINE:
+		alreadyPlayed[voiceline] = false
