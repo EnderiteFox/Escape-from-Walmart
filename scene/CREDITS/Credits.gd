ab74e739bc29acf2adadbfba60e473a40308249e
EndersteveGamer <85014963+EndersteveGamer@users.noreply.github.com>
2024-03-26 19:16:41 +0100

    Remade the credits
    Remade the credits to allow adding images
    Made the cursor invisible during the credits
    Changed once again the credits music
    Removed the old credits
    Edited credits roles a little

--- scene/CREDITS/Credits.gd
+++ scene/CREDITS/Credits.gd
@@ -1,7 +1,26 @@
 extends Node3D
 
+@onready var menu: PackedScene = preload("res://scene/Menu.tscn")
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+@onready var credits: VBoxContainer = $Credits
+
+const SCROLL_SPEED: float = 100
+const FAST_SCROLL_MULTIPLIER: float = 5
+const END_MARGIN: int = 50
 
 func _ready() -> void:
-	animationPlayer.get_animation("walking_roomba").loop_mode = Animation.LOOP_LINEAR
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	animationPlayer.get_animation("walking_roomba") \
+		.loop_mode = Animation.LOOP_LINEAR
 	animationPlayer.play("walking_roomba")
+	credits.position.y = DisplayServer.screen_get_size().y
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Back"):
+		get_tree().change_scene_to_packed(menu)
+	credits.position.y -= SCROLL_SPEED * delta \
+		* (FAST_SCROLL_MULTIPLIER if Input.is_action_pressed("CreditFastScroll")
+			else 1.0)
+	var last_node: Control = credits.get_children()[-1]
+	if (last_node.global_position.y + last_node.size.y + END_MARGIN < 0):
+		get_tree().change_scene_to_packed(menu)
